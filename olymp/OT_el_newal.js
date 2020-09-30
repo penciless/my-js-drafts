1734c9144cce38e6e2fb99248a841075423fad63
penciless <binh.ng1195@gmail.com>
2020-10-01 01:43:34 +0700

    Class to update market

--- olymp/OT_el_newal.js
+++ olymp/OT_el_newal.js
@@ -1,31 +1,3 @@
-function candles(numCandles) {
-  var $candlesPath = document
-    .querySelectorAll(".main-chart.main-chart_candle")[0]
-    .getElementsByTagName("path")[2];
-  var data = $candlesPath
-    .getAttribute("d")
-    .split(/ZM|[ZM]/)
-    .filter(Boolean);
-  var candlesChart = [];
-  var index = numCandles ? data.length - numCandles : 0;
-
-  for (var i = index; i < data.length; i++) {
-    const
-    cords = data[i].split("L"),
-    Y1 = parseFloat(cords[1].split(",")[1]),
-    Y2 = parseFloat(cords[2].split(",")[1]),
-    YDelta = Math.abs(Y2 - Y1);
-
-    YDelta > 1 ?
-      candlesChart.push("up") :
-      YDelta === 1 ?
-      candlesChart.push("standoff") :
-      candlesChart.push("down");
-  }
-  
-  return candlesChart;
-}
-
 // #########
 
 function scanChart() {
@@ -148,42 +120,119 @@ function trade(direction, isWin) {
   console.log("Make a trade order!!!");
 }
 
+// #########
+
+function Market(initConfig) {
+  const data = {};
+  Market.data = function (key) {
+    return key ? data[key] : data;
+  };
+
+  const config = initConfig || {};
+  Market.config = function () {
+    return config;
+  };
+  Market.config.candles = function (number) {
+    config.candles = number;
+    return this;
+  };
+
+  const candles = Market.candles = function (numCandles) {
+    var
+    $candlesPath = document.querySelectorAll(".main-chart.main-chart_candle")[0].getElementsByTagName("path")[2],
+    data = $candlesPath.getAttribute("d").split(/ZM|[ZM]/).filter(Boolean),
+    index = numCandles ? data.length - numCandles : 0,
+    candlesChart = [];
+
+    for (var i = index; i < data.length; i++) {
+      const
+      cords = data[i].split("L"),
+      Y1 = parseFloat(cords[1].split(",")[1]),
+      Y2 = parseFloat(cords[2].split(",")[1]),
+      YDelta = Math.abs(Y2 - Y1);
+
+      YDelta > 1 ?
+        candlesChart.push("up") :
+        YDelta === 1 ?
+          candlesChart.push("standoff") :
+          candlesChart.push("down");
+    }
+
+    return candlesChart;
+  };
+
+  const dealActive = Market.dealActive = function () {
+    const deal = document.querySelector(".deal-card");
+    if (!deal || !deal.hasAttribute("data-id")) return {};
+
+    const
+    dealId = deal.attributes["data-id"].value,
+    $returnRate = deal.querySelector(".deal-card-title__subtitle span"),
+    $betDownDirection = deal.querySelector(".deal-card-amount__pic_down"),
+    $betAmount = deal.querySelector(".deal-card-amount__value"),
+    $statusWin = deal.querySelector(".colored-amount_win"),
+    $statusLose = deal.querySelector(".colored-amount_loose"),
+    profitText = deal.querySelector(".colored-amount span").innerText;
+
+    return {
+      id: dealId,
+      percentReturnRate: parseInt($returnRate.innerText, 10),
+      betDirection: $betDownDirection ? "down" : "up",
+      betAmount: parseFloat($betAmount.innerText.match(/[.\d]*$/)[0]),
+      status: $statusWin ? "win" : $statusLose ? "lose" : "draw",
+      profit: parseFloat(profitText[0] + profitText.match(/[.\d]*$/)[0])
+    };
+  };
+
+  const dealHistory = Market.dealHistory = function() {
+    const deal = document.querySelector(".card_history");
+    if (!deal || !deal.hasAttribute("data-id")) return {};
+
+    const dealId = deal.attributes["data-id"].value,
+    $percentReturnRate = deal.querySelector(".card__subtitle-winperc"),
+    $betDownDirection = deal.querySelector(".icon-trade-dir_down"),
+    $betAmount = deal.querySelector(".card__amount-sum__text span"),
+    $statusWin = deal.querySelector(".colored-amount_win"),
+    $statusLose = deal.querySelector(".colored-amount_loose"),
+    profitText = deal.querySelector(".colored-amount span").innerText;
+
+    return {
+      id: dealId,
+      percentReturnRate: parseInt($percentReturnRate.innerText, 10),
+      betDuration: deal.querySelector(".card__duration").innerText,
+      betDirection: $betDownDirection ? "down" : "up",
+      betAmount: parseFloat($betAmount.innerText.match(/[.\d]*$/)[0]),
+      status: $statusWin ? "win" : $statusLose ? "lose" : "draw",
+      profit: parseFloat(profitText[0] + profitText.match(/[.\d]*$/)[0])
+    };
+  }
+
+  const update = Market.update = function () {
+    const activeDeal = dealActive();
+    data.candles = candles(config.candles);
+    data.dealActive = activeDeal;
+    data.lastDealActiveId = activeDeal.id ? activeDeal.id : data.lastDealActiveId;
+    data.dealHistory = dealHistory();
+  };
+
+  update();
+  return data;
+}
+
+new Market({ candles: 4 });
+Market.config.candles(4);
+Market.update();
+Market.data();
+
 /***********************/
 var _intervalID = 0;
 function main() {
-  var isTrading = false;
-  var winDirection = null;
-  var stopMatchTime = false;
   _intervalID = setInterval(function () {
-    var chartInfo = scanChart();
-
-    if (stopMatchTime && chartInfo.matchTime) {
-      console.log("return");
-      return;
-    } else {
-      console.log("continue");
-      stopMatchTime = false;
+    updateMarket();
+    if (condition()) {
+      trade();
     }
-
-    // Init trade
-    if (!isTrading && chartInfo.matchTime && chartInfo.matchPattern) {
-      console.log("######### Init Trade @ ", Date.now());
-      trade(chartInfo.direction, true);
-      winDirection = chartInfo.direction;
-      isTrading = true;
-      stopMatchTime = true;
-    } else if (isTrading && chartInfo.matchTime && !stopMatchTime) {
-      console.log("trading exam");
-      if (winDirection == chartInfo.direction) {
-        console.log("Win @ ");
-        isTrading = false;
-      } else {
-        console.log("Lose @ ");
-        trade(chartInfo.direction, false);
-        winDirection = chartInfo.direction;
-      }
-    }
-  }, 100);
+  });
 }
 
 clearInterval(_intervalID);
